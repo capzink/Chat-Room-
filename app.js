ac9d68aed6a79dc1ca3893912d7a95f4725c4338
Carlos <capz2003@gmail.com>
2022-01-13 08:21:46 -0500

    sets htpp and socket

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
+
 const app = express()
-const port = process.env.port || 3000
-
+const server = http.createServer(app)
+const io = socketio(server)
 //statis folder
-
 app.use(express.static(path.join(__dirname, 'public')))
 
+//run with client connect
+io.on('connection', socket =>{
+    console.log('New websocket connection');
+})
+
+const port = process.env.port || 3000
 const start = ()=>{
     app.listen(port, ()=>{
         console.log('listening on port', port);
